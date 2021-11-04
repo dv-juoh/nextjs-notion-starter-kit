c9bb70969525224ccb343a01e5dcde82d1cd8f0d
윤주오 <yjo0512@midasin.com>
2021-11-04 11:55:47 +0900

    feat : 불필요한것들 정리

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-
+  rootNotionPageId: '685cc5a556b8499eb7c6690beb832f50',
+  //https://yjo0512.notion.site/JUOH-DV-BLOG-685cc5a556b8499eb7c6690beb832f50
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'DV JUOH BLOG',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'JUOH',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'DV JUOH BLOG',
+  //socialImageTitle: 'Transitive Bullshit',
+  //socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //  twitter: 'transitive_bs',
+  //github: 'dv-juoh',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
